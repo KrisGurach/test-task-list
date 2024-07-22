2074b20c140af0ee29822827195f2d53f6f1f520
Kristina Gurachenkova <gurachenkova@yandex.ru>
2024-07-22 10:30:47 +0300

    create form for to do list

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 const App = () => {
-
+   return (
+  <div className="container">
+    <h1>Список задач</h1>
+    <form>
+      <input
+        type="text"
+        placeholder="Заголовок"
+        required
+      />
+      <input
+        type="text"
+        placeholder="Описание"
+        required
+      />
+      <input
+        type="date"
+        required
+      />
+      <button type="submit">Сохранить</button>
+    </form>
+    <ul>
+    </ul>
+  </div>
+   );
 };
 
 export default App;
